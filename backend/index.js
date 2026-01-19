0e537c32cb6875989ba010e9c279abebb452d048
Aman Kumar <ar1075840@gmail.com>
2026-01-19 21:42:27 +0530

    index fixed

--- backend/index.js
+++ backend/index.js
@@ -67,11 +67,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// const corsOptions = {
-//   origin: "http://localhost:5173",
-//   credentials: true,
-// };
-// app.use(cors(corsOptions));
 
 const corsOptions = {
   origin: [
